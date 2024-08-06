0cd090fdc212ed35f3843a573965b917a068962d
mert_gvcn <mertguvercin2942@gmail.com>
2024-08-06 13:54:56 +0300

    redux ve toaster installation

--- Presentation/OnionArch.Web/web/src/index.tsx
+++ Presentation/OnionArch.Web/web/src/index.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+//redux
+import { Provider } from 'react-redux';
+import { store } from './redux/app/store';
+//toaster
+import { Toaster } from 'react-hot-toast';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
+    <Toaster />
     <App />
-  </React.StrictMode>
+  </Provider>
 );
